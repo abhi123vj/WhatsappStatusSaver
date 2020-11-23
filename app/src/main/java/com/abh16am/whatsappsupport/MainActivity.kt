8b20182a531baddc8900642c88051f560101b2a1
4bh1 <abhiram123vj@gmail.com>
2020-11-24 01:27:24 +0530

    Nothing

--- app/src/main/java/com/abh16am/whatsappsupport/MainActivity.kt
+++ app/src/main/java/com/abh16am/whatsappsupport/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
 
-//hi
 class MainActivity : AppCompatActivity() {
     lateinit var recylerHome : RecyclerView
 
