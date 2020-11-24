defe938e6e3d4075d5838497e39dcfe54f491f23
4bh1 <abhiram123vj@gmail.com>
2020-11-24 17:48:42 +0530

    Fragments Added

--- app/src/main/java/com/abh16am/whatsappsupport/MainActivity.kt
+++ app/src/main/java/com/abh16am/whatsappsupport/MainActivity.kt
@@ -2,15 +2,40 @@ package com.abh16am.whatsappsupport
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.abh16am.whatsappsupport.fragment.page1
+import com.abh16am.whatsappsupport.fragment.page2
+import com.abh16am.whatsappsupport.fragment.page3
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var recylerHome : RecyclerView
-
-    lateinit var  layoutManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firstFragment=page1()
+        val secondFragment=page2()
+        val thirdFragment=page3()
+
+        bottomNavigationView.selectedItemId = R.id.page_2
+        setCurrentFragment(secondFragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.page_1->setCurrentFragment(firstFragment)
+                R.id.page_2->setCurrentFragment(secondFragment)
+                R.id.page_3->setCurrentFragment(thirdFragment)
+
+            }
+            true
     }
+}
+
+    private fun setCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
 }
